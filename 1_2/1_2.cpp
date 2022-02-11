39bb361bb67fa35f91cc8d89603c4ace144d6895
RogachevMD <rogachev.md@gmail.com>
2022-02-11 16:54:02 +0400

    1'st lab done

--- 1_2/1_2.cpp
+++ 1_2/1_2.cpp
@@ -5,28 +5,34 @@ int main(int argc, char* argv[])
 {
 	int rank, size, resultLen;
 	char name[MPI_MAX_PROCESSOR_NAME];
-	int msg{};
 	int root = 0;
-
+	int msg{};
 	MPI_Init(&argc, &argv);
 	MPI_Comm_size(MPI_COMM_WORLD, &size);
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 	MPI_Get_processor_name(name, &resultLen);
+	//printf("%d", size);
+	do {
 
-	do
-	{
-		fflush(stdout);
-		if (rank == root)
-		{
+		//fflush(stdout);
 
-			printf("MSG: ");
+		if (rank == root) {
+			//printf("Process %3d on %s: %d old msg\n", rank, name, msg);
+			//fflush(stdout);
 			scanf_s("%d", &msg);
-			fflush(stdout);
+			printf("MSG: %4d\n", msg);
+			printf("Process %3d on %s: %d\n", rank, name, msg);
+			//fflush(stdout);
+
 		}
 
 		MPI_Bcast(&msg, 1, MPI_INT, root, MPI_COMM_WORLD);
-		printf("Process %3d on %s: %d\n", rank, name, msg);
-		//fflush(stdout);
+
+		if (rank != root) {
+			printf("Process %3d on %s: %d\n", rank, name, msg);
+		}
+
+		fflush(stdout);
 
 	} while (msg > 0);
 
---
+++ 1_3/1_3.cpp
@@ -0,0 +1,53 @@
+#include <mpi.h>
+#include <iostream>
+
+int main(int argc, char* argv[])
+{
+	int rank, size, resultLen;
+	char name[MPI_MAX_PROCESSOR_NAME];
+	int root = 0;
+	int msg{};
+
+	MPI_Init(&argc, &argv);
+	MPI_Comm_size(MPI_COMM_WORLD, &size);
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	MPI_Get_processor_name(name, &resultLen);
+	//root = size / 2;
+
+	int fromTgt = ((rank == 0 ? size : rank)) - 1;
+	int toTgt = (rank == size - 1) ? 0 : rank + 1;
+
+	int bufferSize = 1;
+	int msgTag = 10;
+
+	if (rank == root)
+	{
+		msg = 1000;
+
+		MPI_Send(&msg, bufferSize, MPI_INT, toTgt, msgTag,
+			MPI_COMM_WORLD);
+
+		MPI_Recv(&msg, bufferSize, MPI_INT, fromTgt, msgTag,
+			MPI_COMM_WORLD, MPI_STATUSES_IGNORE);
+
+		printf("msg from %3d to %3d: %d", fromTgt, rank, msg);
+		fflush(stdout);
+
+	}
+	else
+	{
+		MPI_Recv((&msg), bufferSize, MPI_INT, fromTgt, msgTag,
+			MPI_COMM_WORLD, MPI_STATUSES_IGNORE);
+
+		printf("msg from %3d to %3d: %d", fromTgt, rank, msg);
+		fflush(stdout);
+		msg++;
+		MPI_Send(&msg, bufferSize, MPI_INT, toTgt, msgTag,
+			MPI_COMM_WORLD);
+	}
+
+
+	MPI_Finalize();
+	return 0;
+}
+
